d574880b51f44925e77b23dcff78e0001d229f53
Elias Vanderstuyft <elias.vds@gmail.com>
2015-02-17 16:40:35 +0100

    Document construction of set of equations for triangulation. (2)

--- Draft/SLAMTests/FastFeatureDetector_OptFlowMatching/slam2.py
+++ Draft/SLAMTests/FastFeatureDetector_OptFlowMatching/slam2.py
@@ -90,7 +90,7 @@ def iterative_LS_triangulation(u, P, u1, P1):
     Iterative (Linear) Least Squares based triangulation.
     From "Triangulation", Hartley, R.I. and Sturm, P., Computer vision and image understanding, 1997.
     
-    (u, P) is the reference pair containing homogenous image coordinates (x, y) and the corresponding camera matrix.
+    (u, P) is the reference pair containing normalized image coordinates (x, y) and the corresponding camera matrix.
     (u1, P1) is the second pair.
     
     Additionally returns a status-vector to indicate outliers:
@@ -112,7 +112,19 @@ def iterative_LS_triangulation(u, P, u1, P1):
     C1 = np.array(iterative_LS_triangulation_C)
     
     for xi in range(len(u)):
-        # Build C matrices, to visualize calculation structure of A and b
+        # Derivation of matrices A and b:
+        # for each camera following equations hold in case of perfect point matches:
+        #     u.x * (P[2,:] * x)     =     P[0,:] * x
+        #     u.y * (P[2,:] * x)     =     P[1,:] * x
+        # and imposing the constraint:
+        #     x = [x.x, x.y, x.z, 1]^T
+        # yields:
+        #     (u.x * P[2, 0:3] - P[0, 0:3]) * [x.x, x.y, x.z]^T     +     (u.x * P[2, 3] - P[0, 3]) * 1     =     0
+        #     (u.y * P[2, 0:3] - P[1, 0:3]) * [x.x, x.y, x.z]^T     +     (u.y * P[2, 3] - P[1, 3]) * 1     =     0
+        # and since we have to do this for 2 cameras, and since we imposed the constraint,
+        # we have to solve 4 equations in 3 unknowns (in LS sense).
+
+        # Build C matrices, to construct A and b in a concise way
         C[:, 2] = u[xi, :]
         C1[:, 2] = u1[xi, :]
         
