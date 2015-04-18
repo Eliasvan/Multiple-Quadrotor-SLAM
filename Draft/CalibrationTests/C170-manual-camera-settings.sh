5f8807637fdc4063b328562dc6b957b5b5b9413b
Elias Vanderstuyft <elias.vds@gmail.com>
2015-04-18 11:50:06 +0200

    Rename CalibrationTests.

--- Draft/CalibrationTests/C170-manual-camera-settings.sh
+++
@@ -1,7 +0,0 @@
-# These commands maximize exposure at the device's max fps (= 30)
-
-v4l2-ctl --device=$1 \
-	--set-ctrl=exposure_auto=1 \
-	--set-ctrl=white_balance_temperature_auto=0
-v4l2-ctl --device=$1 \
-	--set-ctrl=exposure_absolute=78
