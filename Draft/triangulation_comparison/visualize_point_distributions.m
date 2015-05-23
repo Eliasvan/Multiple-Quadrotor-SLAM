d7c7c35f8dcda668d65dba1b6f60f15714701833
Elias Vanderstuyft <elias.vds@gmail.com>
2015-05-23 20:12:13 +0200

    Add some Blender renders of the point distributions, and make some figures more readable.

--- Draft/triangulation_comparison/visualize_point_distributions.m
+++ Draft/triangulation_comparison/visualize_point_distributions.m
@@ -1,4 +1,11 @@
-FontSize = 20;
+% Set easy-to-read metrics, fonts, ...
+
+set(0, 'DefaultAxesFontName', 'DejaVuSans')
+set(0, 'DefaultTextFontname', 'DejaVuSans')
+set(0, 'DefaultFigurePaperUnits', 'inches')
+set(0, 'DefaultFigurePaperSize', [4, 4])
+set(0, 'DefaultFigurePaperPosition', [0 0 [4, 4]])
+set(0, 'DefaultLineMarkerSize', 6)
 
 
 % Finite points (in sphere arrangement)
@@ -13,14 +20,12 @@ max_towards = 12.;
 points_3D = finite_3D_points * points_r;
 
 figure(1)
-plot(points_3D(:, 1), points_3D(:, 3), 'b.', 'MarkerSize',12)
-set(gca, 'FontSize', FontSize)
+plot(points_3D(:, 1), points_3D(:, 3), 'b.')
 xlabel('X-coordinate of 3D point')
 ylabel('Z-coordinate of 3D point')
 saveas(gcf, ['figures/points_3D_finite_top.pdf'])
 figure(2)
-plot(points_3D(:, 1), points_3D(:, 2), 'b.', 'MarkerSize',12)
-set(gca, 'FontSize', FontSize)
+plot(points_3D(:, 1), points_3D(:, 2), 'b.')
 set(gca, 'YDir', 'reverse')
 xlabel('X-coordinate of 3D point')
 ylabel('Y-coordinate of 3D point')
@@ -39,14 +44,12 @@ max_towards = 11.;
 points_3D = scene_3D_points * points_r;
 
 figure(3)
-plot(points_3D(:, 1), points_3D(:, 3), 'b.', 'MarkerSize',12)
-set(gca, 'FontSize', FontSize)
+plot(points_3D(:, 1), points_3D(:, 3), 'b.')
 xlabel('X-coordinate of 3D point')
 ylabel('Z-coordinate of 3D point')
 saveas(gcf, ['figures/points_3D_scene_top.pdf'])
 figure(4)
-plot(points_3D(:, 1), points_3D(:, 2), 'b.', 'MarkerSize',12)
-set(gca, 'FontSize', FontSize)
+plot(points_3D(:, 1), points_3D(:, 2), 'b.')
 set(gca, 'YDir', 'reverse')
 xlabel('X-coordinate of 3D point')
 ylabel('Y-coordinate of 3D point')
